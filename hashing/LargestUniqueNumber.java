541aaad2025c5734b49b90c4aca0463e829012c0
Preet Sahu <preetswork724@gmail.com>
2026-01-09 13:59:14 +0530

    Add Largest Unique Number (frequency count) under hashing

---
+++ hashing/LargestUniqueNumber.java
@@ -0,0 +1,66 @@
+/**
+ * Problem:
+ * <Largest Unique Number>
+ *
+ * Link:
+ * <https://algo.monster/liteproblems/1133>
+ *
+ * Pattern:
+ * <Frequency count>
+ *
+ * Brute Force Intuition:
+ * - For all the numbers in the array, count the occurrence of each number.
+ * - If the number of occurrence is 1 then update the maximum.
+ *
+ * - Why it is inefficient?
+ * - Rescans the whole array for every single number.
+ * - Quiet slow.
+ *
+ * Time Complexity:
+ * - O(N^2)
+ * Space Complexity:
+ * - O(1)
+ *
+ * Better Approach Intuition:
+ * - Use a map and store numbers as key and their frequency as the value to the key.
+ * - Using a map gives the frequency of a number in O(1).
+ * - Iterate over the mop, if the frequency of the number is 1, then update the maximum.
+ *
+ * Time Complexity:
+ * - O(N)
+ * Space Complexity:
+ * - O(N)
+ *
+ * Why this is still not optimal?
+ * - Map uses extra space proportional to input.
+ * - Slower constants than array.
+ *
+ * Optimal Approach (used below):
+ * - Small constraints allow array counting, therefore we can replace a map with a fixed-size array and achieve it in O(1) space.
+ *
+ * Time Complexity:
+ * - O(N)
+ * Space Complexity:
+ * - O(1)
+ *
+ * Notes:
+ * - Frequency problems -> HashMap first, then optimize to array is constraints are small.
+ */
+package hashing;
+
+public class LargestUniqueNumber {
+    public static void main(String[] args) {
+        System.out.println(largestUnque(new int[]{9, 9, 8, 8}));
+    }
+
+    public static int largestUnque(int[] nums){
+        int[] freq = new int[1001];
+
+        for(int num : nums) freq[num]++;
+
+        for(int number = 1000 ; number >= 0; number--){
+            if(freq[number] == 1) return number;
+        }
+        return -1;
+    }
+}
