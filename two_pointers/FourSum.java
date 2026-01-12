60213721e5bff8f2b977c088569f9949b7bc4f26
Preet Sahu <preetswork724@gmail.com>
2026-01-12 18:52:39 +0530

    Add correct better approach intuition and time complexity for Triplets with Smaller Sum

--- two_pointers/FourSum.java
+++ two_pointers/FourSum.java
@@ -72,11 +72,11 @@ public class FourSum {
 
         for(int i = 0; i < n; i++){
 
-            if(i > 0 && nums[i] == nums[i-1]) continue;
+            if(i > 0 && nums[i] == nums[i-1]) continue; // Skip duplicates
 
             for(int j = i+1; j < n; j++){
 
-                if(j > i+1 && nums[j] == nums[j-1]) continue;
+                if(j > i+1 && nums[j] == nums[j-1]) continue; // Skip duplicates
 
                 int left = j + 1, right = n - 1;
 
@@ -87,8 +87,8 @@ public class FourSum {
                     if(sum == target){
                         res.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[left], nums[right])));
 
-                        while(left < right && nums[left] == nums[left + 1]) left++;
-                        while(left < right && nums[right] == nums[right - 1]) right--;
+                        while(left < right && nums[left] == nums[left + 1]) left++; // Skip duplicates
+                        while(left < right && nums[right] == nums[right - 1]) right--; // Skip duplicates
                         left++;
                         right--;
                     }
