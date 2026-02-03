598fd40d966c27232feacf52c588b6f526fb471c
Preet Sahu <preetswork724@gmail.com>
2026-02-03 17:25:44 +0530

    Add Rotate Array Counter Clockwise (rotate array) under arrays

--- arrays/RotateArrayCounterClockwise.java
+++ arrays/RotateArrayCounterClockwise.java
@@ -55,8 +55,32 @@
 
 package arrays;
 
+import java.util.Arrays;
+
 public class RotateArrayCounterClockwise {
     public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        int d = 2;
+        rotateArr(arr, d);
+        System.out.println(Arrays.toString(arr));
+    }
 
+    static void rotateArr(int arr[], int d) {
+        int n  = arr.length;
+
+        reverse(arr, 0, d-1);
+        reverse(arr, d, n-1);
+        reverse(arr, 0, n-1);
+    }
+
+    static void reverse(int[] arr, int left, int right){
+
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
     }
 }
