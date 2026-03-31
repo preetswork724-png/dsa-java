98ede5549409f946f24e801ba4eea969ee85d270
Preet Sahu <preetswork724@gmail.com>
2026-03-31 23:27:42 +0530

    Add Node class under trees

---
+++ trees/Node.java
@@ -0,0 +1,12 @@
+package trees;
+
+public class Node {
+    public int data;
+    public Node left, right;
+
+    Node(int val) {
+        this.data = val;
+        this.left = null;
+        this.right = null;
+    }
+}
