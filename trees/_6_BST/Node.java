841c86d8eb938d6fc36e6c0f5901dab507b203a5
Preet Sahu <preetswork724@gmail.com>
2026-06-12 20:09:30 +0530

    Add Node class

---
+++ trees/_6_BST/Node.java
@@ -0,0 +1,13 @@
+package trees._6_BST;
+
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
